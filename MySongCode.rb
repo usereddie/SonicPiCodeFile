067bf53c978c8d1a6e5064d2674337c50bef5c39
usereddie <160058055+usereddie@users.noreply.github.com>
2024-02-15 11:36:45 -0800

    Create MySongCode.rb

---
+++ MySongCode.rb
@@ -0,0 +1,127 @@
+# Spongebob Challenge
+# CHALLENGE D: Use functions to optimize the SpongeBob theme!
+
+use_bpm 136
+use_synth :piano
+
+define :purple_notes do
+  play:E4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 2
+  play:E5
+  play:Gs4
+  sleep 1
+end
+
+define :yellow_notes do
+  play :r
+  sleep 1
+  play:E5
+  play:Gs4
+  play:B4
+  sleep 1
+  play:B4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 1
+end
+
+define :blue_notes do
+  play:B4
+  sleep 0.75
+  play:As4
+  sleep 0.25
+  play:Gs4
+  play:B4
+  sleep 0.75
+  play:Cs5
+  sleep 0.25
+  play:B4
+  sleep 1
+  play:Gs4
+  play:E5
+  sleep 1
+end
+
+
+live_loop:background_notes do
+  play:E3, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  play:E4, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  sleep 1
+  play:E4, amp: 0.25
+  sleep 1
+  
+  play:E3, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  play:E4, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  sleep 1
+  play:E4, amp: 0.5
+  sleep 1
+  
+  play:E3, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  play:E4, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  sleep 1
+  play:E4, amp: 0.75
+  sleep 1
+  
+  sleep 1
+ 
+  5.times do
+    play:E3, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    play:E4, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    sleep 1
+    play:E4, amp: 1
+    sleep 1
+  end
+  stop
+end
+
+live_loop :heartbeat do
+  sample :drum_heavy_kick, amp: 0.75
+  sleep 0.5
+  sample :drum_heavy_kick, amp: 0.75
+  sleep 3.5
+end
+
+# Measure 1
+
+# Measure 2
+purple_notes
+
+# Measure 3
+yellow_notes
+
+# Measure 4
+yellow_notes
+
+# Measure 5
+blue_notes
+
+# Measure 6
+yellow_notes
+
+# Measure 7
+blue_notes
+
+# Measure 8
+purple_notes
+
+sample :vinyl_rewind
